fa54f9e8b81d9f48452225379ae171307937835a
Leonardo Wiedemeier <leonardo.wiedemeier@gmail.com>
2025-02-24 17:32:11 +0100

    feat: binary converter

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -1,15 +1,57 @@
 package ch.fhnw.algd1.converters.binary;
 
 public class BinConverter {
-	public static String toString(int x) {
-		// TODO expect x to be in range [-128, 127], return string with 8 binary
-		// digits representing x in 2-complement
-		return "00000000";
-	}
+    public static String toString(int x) {
+        if (x == 0) {
+            return "00000000";
+        }
 
-	public static int fromString(String text) {
-		// TODO expect text to contain 8 binary digits, parse to int value in
-		// 2-complement
-		return 0;
-	}
+        if (x < 0) {
+            x = (1 << 8) - (~x + 1);
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        while (x > 0) {
+            if ((x & 1) == 0) {
+                builder.append(0);
+            } else {
+                builder.append(1);
+            }
+
+            x = x / 2;
+        }
+
+        //in this solution, positive numbers need to be appended with zeroes to reach a 8bit representation
+        if (builder.length() < 8) {
+            char append = '0';
+
+            int counter = 8 - builder.length();
+            for (int i = 0; i < counter; i++) {
+                builder.append(append);
+            }
+        }
+
+        return builder.reverse().toString();
+    }
+
+    public static int fromString(String text) {
+        int res = 0;
+        StringBuilder builder = new StringBuilder(text);
+
+        if ('1' == builder.charAt(0)) {
+            res = ~(1 << 7) + 1;
+        }
+
+        builder.reverse();
+
+        for (int i = 0; i < 7; i++) {
+            char ch = builder.charAt(i);
+            if (ch == '1') {
+                res = res + (1 << i);
+            }
+        }
+
+        return res;
+    }
 }
